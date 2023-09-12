a1fbdb9a02a1265418d1aac38869bd2781da60fd
Angela <avenable11@ivytech.edu>
2023-09-11 20:07:50 -0400

    sentinel controlled while loop 9/11/2023

--- 09_11_2023/main.cpp
+++ 09_11_2023/main.cpp
@@ -6,21 +6,26 @@ int main()
     double day;
     double sum = 0;
     int i = 0;
-    while (i < 7)
+    char again = 'y';
+    while (again == 'y')
     {
-        std::cout << "Enter the calories for day " << i << ": ";
+        std::cout << "Enter the calories for day " << ++i << ": ";
         std::cin >> day;
+        std::cout << std::endl;
         if (!std::cin)
         {
             return 0;
         }
-        sum += day;       // sum = sum + day;
-        /*int x = */ i++; // x = i;i = i + 1;
+        sum += day; // sum = sum + day;
+        /*int x = i++; // x = i;i = i + 1; */
         /* x = i--;
         x = ++i; // i = i + 1; x = i;
         x = --i; */
+        std::cout << "Do you want to enter another day? ";
+        std::cin >> again;
+        std::cout << std::endl;
     }
-
-    std::cout << "The average calories burned per day is " << sum / 7 << std::endl;
+    std::cout << "You entered data for " << i << " days." << std::endl;
+    std::cout << "The average calories burned per day is " << sum / i << std::endl;
     return 0;
 }
