68619b9f3c9d79082c635395cf384319c8c16a4c
Angela <avenable11@ivytech.edu>
2023-09-18 22:25:50 -0400

    final program with lecture activity 9/18/2023

--- 09_18_2023/main.cpp
+++ 09_18_2023/main.cpp
@@ -3,13 +3,16 @@
 #include <cmath>
 #include <algorithm>
 #include <string>
+#include <climits>
+#include <cassert>
 
-bool validateUnits(std::string units)
-{
-    transform(units.begin(), units.end(), units.begin(), ::toupper);
-    bool valid = units == "B" || units == "KB" || units == "MB" || units == "GB" || units == "TB";
-    return valid;
-}
+bool validateUnits(std::string u);
+long long calculateMemCells(int, std::string);
+int secret(int);
+void resetStream();
+void inputInt(std::istream &, int &, std::string, int, int = INT_MAX);
+void codeGradeLoopFix(std::string errorLocation);
+int count();
 
 int main()
 {
@@ -17,58 +20,112 @@ int main()
     std::string units;
     int mem;
 
-    std::cout << "Enter the memory size: ";
-    std::cin >> mem;
-    std::cout << std::endl;
-    while (!std::cin || mem < 0)
-    {
-        if (!std::cin)
-        {
-            std::cout << "You entered something that is not a number." << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-        std::cout << "The value entered is invalid. Please enter a number greater than 0." << std::endl;
-        std::cout << "Enter the memory size: ";
-        std::cin >> mem;
-        std::cout << std::endl;
-    }
+    inputInt(std::cin, mem, "Enter the memory size: ", 0);
+
     std::cout << "Enter the memory units(B, KB, MB, GB, TB): ";
     std::cin >> units;
     std::cout << std::endl;
-    std::string upperUnits = units;
-    transform(units.begin(), units.end(), upperUnits.begin(), ::toupper);
-    while (upperUnits != "B" && upperUnits != "KB" && upperUnits != "MB" && upperUnits != "GB" && upperUnits != "TB")
+    while (!validateUnits(units))
     {
+        codeGradeLoopFix("Line 29");
         std::cout << "Your entry is not a valid memory unit. Valid units are B, KB, MB, GB, TB." << std::endl;
         std::cout << "Enter the memory units(B, KB, MB, GB, TB): ";
         std::cin >> units;
         std::cout << std::endl;
-        upperUnits = units;
-        transform(units.begin(), units.end(), upperUnits.begin(), ::toupper);
     }
+    memSize = calculateMemCells(mem, units);
+    std::cout << "The minimum MAR needed is " << ceil(log2(memSize)) << " bits." << std::endl;
+    std::cout << secret(7) << std::endl;
+    std::cout << secret(3) << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << count() << std::endl;
+    }
+
+    return 0;
+}
+
+bool validateUnits(std::string u)
+{
+    transform(u.begin(), u.end(), u.begin(), ::toupper);
+    bool valid = u == "B" || u == "KB" || u == "MB" || u == "GB" || u == "TB";
+    return valid;
+}
+
+long long calculateMemCells(int memAmount, std::string units)
+{
+    long long memSize;
     switch (units[0])
     {
     case 'k':
     case 'K':
-        memSize = mem * pow(2, 10);
+        memSize = memAmount * pow(2, 10);
         break;
     case 'm':
     case 'M':
-        memSize = mem * pow(2, 20);
+        memSize = memAmount * pow(2, 20);
         break;
     case 'g':
     case 'G':
-        memSize = mem * pow(2, 30);
+        memSize = memAmount * pow(2, 30);
         break;
     case 'T':
     case 't':
-        memSize = mem * pow(2, 40);
+        memSize = memAmount * pow(2, 40);
         break;
     default:
-        memSize = mem;
+        memSize = memAmount;
     }
-    std::cout << "The minimum MAR needed is " << ceil(log2(memSize)) << " bits." << std::endl;
+    return memSize;
+}
 
-    return 0;
+int secret(int x)
+{
+    if (x > 5)
+        return 2 * x;
+
+    return x;
+}
+
+void resetStream()
+{
+    // std::cout << "You entered something that is not a number." << std::endl;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void inputInt(std::istream &in, int &x, std::string prompt, int min, int max)
+{
+    std::cout << prompt;
+    std::cin >> x;
+    std::cout << std::endl;
+    while (!std::cin || x < min || x > max)
+    {
+        codeGradeLoopFix("Line 98");
+        if (!std::cin)
+        {
+            resetStream();
+        }
+        std::cout << "The value entered is invalid. Please enter a number between " << min << " and " << max << std::endl;
+        std::cout << prompt;
+        std::cin >> x;
+        std::cout << std::endl;
+    }
+}
+
+void codeGradeLoopFix(std::string errorLocation)
+{
+    if (std::cin.eof())
+    {
+        std::cout << "There was a problem and there is no more input! @" << errorLocation << std::endl;
+        assert(!std::cin.eof());
+    }
+}
+
+int count()
+{
+    static int c = 0;
+    // lecture activity do something that increases the value of c by 1.
+
+    return c;
 }
