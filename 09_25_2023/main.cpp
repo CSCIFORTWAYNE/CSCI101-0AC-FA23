a942cd51b2783fbbf857e5716058ed3012c7230c
Angela <avenable11@ivytech.edu>
2023-09-25 20:29:57 -0400

    io functions 9/25/2023

--- 09_25_2023/main.cpp
+++ 09_25_2023/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <limits>
+#include <cassert>
 
 /* const int RED = 0;
 const int BLUE = 1;
@@ -32,9 +35,18 @@ enum cardFaceType
     JOKER = 25
 };
 
+std::string cardFaceToString(cardFaceType);
+std::string changeConsoleColor(colorType);
+colorType inputColor();
+void outputColorList();
+void resetStream();
+void codeGradeLoopFix(std::string errorLocation);
+
 int main()
 {
-    colorType color = BLUE;
+    colorType color = PURPLE;
+    color = inputColor();
+    std::cout << changeConsoleColor(color);
 
     if (color == RED)
     {
@@ -45,6 +57,16 @@ int main()
     cardFaceType c1 = static_cast<cardFaceType>(c + 1);
     std::cout << c << std::endl;
     // std::cin >> c1;
+    std::string output = cardFaceToString(c);
+    std::cout << output << std::endl;
+    output = cardFaceToString(c1);
+    std::cout << output << std::endl;
+
+    return 0;
+}
+
+std::string cardFaceToString(cardFaceType c)
+{
     std::string output;
     switch (c)
     {
@@ -93,8 +115,107 @@ int main()
     default:
         output = "Unknown";
     }
+    return output;
+}
 
-    std::cout << output << std::endl;
+std::string changeConsoleColor(colorType color)
+{
+    std::string output = "\033[1m";
+    switch (color)
+    {
+    case RED:
+        output += "\033[38;5;196m\033[48;5;88m";
+        break;
+    case BLUE:
+        output += "\033[38;5;81m\033[48;5;4m";
+        break;
+    case GREEN:
+        output += "\033[38;5;118m\033[48;5;2m";
+        break;
+    case PURPLE:
+        output += "\033[38;5;135m\033[48;5;53m";
+        break;
+    default:
+        output += "\033[0m";
+    }
+    return output;
+}
 
-    return 0;
+colorType inputColor()
+{
+    int colorInt;
+    std::cout << "Pick a color" << std::endl;
+    outputColorList();
+    std::cin >> colorInt;
+    while (!std::cin || colorInt < 1 || colorInt > 4)
+    {
+        codeGradeLoopFix("line 152");
+        if (!std::cin)
+        {
+            resetStream();
+        }
+        std::cout << "Your entry is invalid." << std::endl;
+        std::cout << "Pick a color" << std::endl;
+        outputColorList();
+        std::cin >> colorInt;
+    }
+    switch (colorInt)
+    {
+    case 1:
+        return RED;
+    case 2:
+        return BLUE;
+    case 3:
+        return GREEN;
+    case 4:
+        return PURPLE;
+    default:
+        return RED;
+    }
+
+    /* std::string inputStr;
+
+    while (true)
+    {
+        std::cout << "Pick a color (Red, Blue, Green, Purple): ";
+        std::cin >> inputStr;
+        transform(inputStr.begin(), inputStr.end(), inputStr.begin(), ::tolower);
+        switch (inputStr[0])
+        {
+        case 'r':
+            return RED;
+        case 'b':
+            return BLUE;
+        case 'g':
+            return GREEN;
+        case 'p':
+            return PURPLE;
+        default:
+            continue;
+        }
+    } */
+}
+
+void outputColorList()
+{
+    std::cout << "1. Red" << std::endl;
+    std::cout << "2. Blue" << std::endl;
+    std::cout << "3. Green" << std::endl;
+    std::cout << "4. Purple" << std::endl;
+}
+
+void resetStream()
+{
+    std::cout << "You entered something that is not a number." << std::endl;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void codeGradeLoopFix(std::string errorLocation)
+{
+    if (std::cin.eof())
+    {
+        std::cout << "There was a problem and there is no more input! @" << errorLocation << std::endl;
+        assert(!std::cin.eof());
+    }
 }
